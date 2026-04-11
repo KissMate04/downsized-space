e6c56a1e5b1f6f52e31cbfd768ac9397ea6e404a
KissMate04 <km.kissmate@gmail.com>
2026-04-11 16:57:24 +0200

    added pygame package installer to launcher

--- launcher.py
+++ launcher.py
@@ -1,4 +1,25 @@
-from downsized_space.main import main
+import importlib
+import sys
+import subprocess
+
+
+def install_requirements():
+    try:
+        import pygame
+    except ImportError:
+        print("Required packages not found. Installing now...")
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+            print("Installation complete.")
+        except subprocess.CalledProcessError:
+            print(
+                "Failed to install requirements automatically. Please run 'pip install -r requirements.txt' manually.")
+            sys.exit(1)
+
 
 if __name__ == '__main__':
+    install_requirements()
+    importlib.invalidate_caches()
+    from downsized_space.main import main
+
     main()
